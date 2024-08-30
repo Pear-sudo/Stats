84c42cbd5374d31f6093336afdc10def028e95c5
adam <adameve1981zero@gmail.com>
2024-08-30 12:35:13 +0800

    Initial Commit

---
+++ Stats/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Stats
+//
+//  Created by A on 30/08/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StatsTests/StatsTests.swift
@@ -0,0 +1,16 @@
+//
+//  StatsTests.swift
+//  StatsTests
+//
+//  Created by A on 30/08/2024.
+//
+
+import Testing
+
+struct StatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
